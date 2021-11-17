13c5c1d9da79cc1c115476ec0dbc2e2f6b065153
Sterben33 <nikitka.gorbunov.1976@mail.ru>
2021-11-17 22:36:14 +0300

    anither method

--- js-ipfs/ipfs.js
+++ js-ipfs/ipfs.js
@@ -1,12 +1,11 @@
-// For using this extention's client you need to download and run 'ipfs daemon' 
-// We have the another one, but I don't clearly understand how to use it yet.
+// For using 'ipfs-http-client's client you need to download and run 'ipfs daemon' 
 const ipfsClent = require('ipfs-http-client');
+const IPFS = require('ipfs-core');
 const fs = require('fs');
 
+// Implementation for 'ipfs-http-client'
 const ipfs = new ipfsClent.create({host: 'localhost', port: '5001', protocol: 'http'});
-
-
-const addFile = async (filePath) => {
+const addFileHttpClient = async (filePath) => {
     const file = fs.readFileSync(filePath);
     const fileAdded = await ipfs.add({content: file});
     console.log(fileAdded)
@@ -14,9 +13,18 @@ const addFile = async (filePath) => {
 
     return fileHash
 }
-
-
 (async () => {
-    const hash = await addFile('Name' ,'t.txt');
-    console.log(`Your file is available on: https://ipfs.io/ipfs/${hash}`)
+    const hash = await addFileHttpClient('t.txt');
+    console.log(`('ipfs-http-client') Your file is available on: https://ipfs.io/ipfs/${hash}`)
 })()
+
+
+// Implementation for 'ipfs-core'
+// const addFileCore = async (filePath) => {
+//     const file = fs.readFileSync(filePath);
+//     const client = await IPFS.create();
+//     const { path } = await client.add(file);
+//     console.log(`('ipfs-core') Your file if awailable on: https://ipfs.io/ipfs/${path}`);
+//     process.exit(); // Need a better way
+// }
+// addFileCore('t.txt');
